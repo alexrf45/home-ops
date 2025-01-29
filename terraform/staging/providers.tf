81d21278ef7d71ded84e3c27ef2db5ba772d0145
alexrf45 <fonalex45@gmail.com>
2025-01-28 21:16:41 -0500

    changing cilium deployment

--- terraform/staging/providers.tf
+++ terraform/staging/providers.tf
@@ -10,7 +10,7 @@ provider "kubernetes" {
 }
 
 provider "proxmox" {
-  endpoint = "https://10.3.3.9:8006"
+  endpoint = "https://${var.pve_endpoint}:8006"
   username = "root@pam"
   password = var.password
   insecure = true
--- terraform/staging/variables.tf
+++ terraform/staging/variables.tf
@@ -1,3 +1,9 @@
+variable "pve_endpoint" {
+  description = "endpoint of pve cluster lead"
+  type        = string
+}
+
+
 variable "password" {
   description = "pve node password"
   type        = string
