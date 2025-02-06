51d84cdce9467d53adb963de3541a44e131f26f9
alexrf45 <fonalex45@gmail.com>
2025-02-05 19:41:34 -0500

    new secret

--- utils/pve-secret.sh
+++ utils/pve-secret.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-kubectl create secret generic proxmox-csi-plugin --from-file=./config.yaml --dry-run=client -o yaml >proxmox-csi-plugin-secrets.yaml
+kubectl create secret generic proxmox-csi-plugin --from-file=./pve-csi-config.yaml \
+  --dry-run=client -o yaml >../infrastructure/controllers/base/proxmox-csi/proxmox-csi-plugin-secrets.yaml
 
-kubectl create secret generic proxmox-cloud-controller --from-file=./config.yaml --dry-run=client -o yaml >proxmox-cloud-controller-secrets.yaml
+kubectl create secret generic proxmox-cloud-controller --from-file=./pve-cloud-config.yaml \
+  --dry-run=client -o yaml >../infrastructure/controllers/base/proxmox-csi/proxmox-cloud-controller-secrets.yaml
