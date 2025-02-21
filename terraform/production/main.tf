e7362e712a894e762c858fbbaddf4c6b33dfe053
alexrf45 <fonalex45@gmail.com>
2025-02-21 00:51:54 -0500

    scaling cluster

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -1,15 +1,15 @@
 module "dev-test" {
   source = "github.com/alexrf45/lab//talos-pve?ref=main"
 
-  pve_nodes             = ["home-2", "home-3", "home-4"]
+  pve_nodes             = ["home-0", "home-1", "home-2", "home-3", "home-4"]
   cert-manager-manifest = "https://github.com/cert-manager/cert-manager/releases/download/v1.16.3/cert-manager.yaml"
   cluster = {
     name          = "prod"
     env           = "prod"
-    endpoint      = "10.3.3.50"
+    endpoint      = "10.3.3.25"
     pve_endpoint  = "10.3.3.2"
     gateway       = "10.3.3.1"
-    talos_version = "v1.9.1"
+    talos_version = "v1.9.4"
     extensions = [
       "intel-ucode",
       "glibc",
@@ -25,88 +25,100 @@ module "dev-test" {
     v1 = {
       install_disk     = "/dev/vda"
       machine_type     = "controlplane"
-      node             = "home-2"
-      vm_id            = 2000
+      node             = "home-0"
+      vm_id            = 1000
       datastore_id     = "local-lvm"
       allow_scheduling = false
-      ip               = "10.3.3.50"
+      ip               = "10.3.3.25"
       cores            = 2
       memory           = 8092
-      size             = 25
+      size             = 50
 
     },
     v2 = {
       install_disk     = "/dev/vda"
       machine_type     = "controlplane"
-      node             = "home-3"
-      vm_id            = 2001
+      node             = "home-1"
+      vm_id            = 1001
       datastore_id     = "local-lvm"
-      ip               = "10.3.3.51"
-      allow_scheduling = false
+      ip               = "10.3.3.26"
+      allow_scheduling = true
       cores            = 2
       memory           = 8092
-      size             = 25
+      size             = 50
 
     },
     v3 = {
       install_disk     = "/dev/vda"
       machine_type     = "controlplane"
-      node             = "home-4"
-      allow_scheduling = false
-      vm_id            = 2002
+      node             = "home-2"
+      allow_scheduling = true
+      vm_id            = 1002
       datastore_id     = "local-lvm"
-      ip               = "10.3.3.52"
+      ip               = "10.3.3.27"
       cores            = 2
       memory           = 8092
-      size             = 25
+      size             = 50
 
     },
     v4 = {
       install_disk = "/dev/vda"
       machine_type = "worker"
-      node         = "home-4"
-      vm_id        = 2003
+      node         = "home-3"
+      vm_id        = 1003
       datastore_id = "local-lvm"
-      ip           = "10.3.3.53"
+      ip           = "10.3.3.28"
       cores        = 2
       memory       = 8092
-      size         = 25
+      size         = 50
 
     },
     v5 = {
       install_disk = "/dev/vda"
       machine_type = "worker"
-      node         = "home-3"
-      vm_id        = 2004
+      node         = "home-4"
+      vm_id        = 1004
       datastore_id = "local-lvm"
-      ip           = "10.3.3.54"
+      ip           = "10.3.3.29"
       cores        = 2
       memory       = 8092
-      size         = 25
+      size         = 50
 
     },
     v6 = {
       install_disk = "/dev/vda"
       machine_type = "worker"
-      node         = "home-2"
-      vm_id        = 2005
+      node         = "home-0"
+      vm_id        = 1005
       datastore_id = "local-lvm"
-      ip           = "10.3.3.55"
+      ip           = "10.3.3.30"
       cores        = 2
       memory       = 8092
-      size         = 25
+      size         = 50
 
     },
     v7 = {
       install_disk = "/dev/vda"
       machine_type = "worker"
-      node         = "home-0"
-      vm_id        = 2006
+      node         = "home-1"
+      vm_id        = 1006
       datastore_id = "local-lvm"
-      ip           = "10.3.3.56"
+      ip           = "10.3.3.31"
       cores        = 2
       memory       = 8092
-      size         = 25
+      size         = 50
+
+    },
+    v8 = {
+      install_disk = "/dev/vda"
+      machine_type = "worker"
+      node         = "home-2"
+      vm_id        = 1007
+      datastore_id = "local-lvm"
+      ip           = "10.3.3.32"
+      cores        = 2
+      memory       = 8092
+      size         = 50
 
     },
   }
