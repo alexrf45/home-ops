2d74b10755974854612fae3df40024cd32a06e1e
alexrf45 <fonalex45@gmail.com>
2025-04-01 21:35:09 -0400

    storage class kube-prometheus-stack to longhorn

--- infra/terraform/talos/prod/bootstrap_prod.sh
+++ infra/terraform/talos/prod/bootstrap_prod.sh
@@ -15,10 +15,10 @@ deploy() {
 
   cp ~/.kube/config ~/.kube/config_bk && KUBECONFIG=~/.kube/dev:~/.kube/prod kubectl config view --flatten >~/.kube/config_tmp && mv ~/.kube/config_tmp ~/.kube/config
 
-  #kubectx admin@prod
+  kubectx admin@prod
 
   #terraform output
-  #kubectl label node prod-node-3 prod-node-4 prod-node-5 prod-node-6 node-role.kubernetes.io/worker=true
+  kubectl label node prod-node-3 prod-node-4 prod-node-5 prod-node-6 node-role.kubernetes.io/worker=true
 
 }
 
@@ -27,11 +27,11 @@ flux-deploy() {
   cat ~/.local/flux-staging.agekey | kubectl create secret generic sops-age \
     --namespace=flux-system \
     --from-file=flux-staging.agekey=/dev/stdin
-  #
+
   flux bootstrap git \
     --cluster-domain=cluster.local \
     --url=ssh://git@github.com/alexrf45/home-ops.git \
-    --path=clusters/production \
+    --path=clusters/prod \
     --private-key-file=/home/fr3d/.ssh/fr3d \
     --branch main
 
@@ -47,3 +47,5 @@ destroy() {
 
   mv ~/.kube/config_bk ~/.kube/config
 }
+
+flux-deploy
