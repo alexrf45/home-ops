395bb4713e7b976acd72759f9aa05047c8b463fb
alexrf45 <fonalex45@gmail.com>
2025-04-17 00:05:05 -0400

    argo and git not cooperating

--- utils/argo-repo-config.sh
+++ utils/argo-repo-config.sh
@@ -4,6 +4,6 @@ kubectl config set-context --current --namespace=argo-cd
 
 argocd repo add git@github.com:alexrf45/prod-argo.git --ssh-private-key-path ~/.ssh/fr3d --upsert
 
-argocd repo add git@github.com:alexrf45/dev-argo.git --ssh-private-key-path ~/.ssh/fr3d
+#argocd repo add git@github.com:alexrf45/dev-argo.git --ssh-private-key-path ~/.ssh/fr3d
 
 kubectl config set-context --current --namespace=default
