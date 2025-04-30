6eb05c02c618c6342aa868450e6a909fa165691c
alexrf45 <fonalex45@gmail.com>
2025-04-30 12:10:47 -0400

    new cluster

--- infra/terraform/talos/prod/outputs.tf
+++ infra/terraform/talos/prod/outputs.tf
@@ -1,14 +1,4 @@
-output "schematic_id" {
-  value = module.prod.schematic_id
-}
 
-output "installer_image_iso" {
-  value = module.prod.installer_image_iso
-}
-
-output "installer_disk_image" {
-  value = module.prod.installer_disk_image
-}
 
 output "client_configuration" {
   value     = module.prod.client_configuration
