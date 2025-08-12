5cbd0a74d4b31c1475a27830584e6464cd8bf9d8
alexrf45 <fonalex45@gmail.com>
2025-08-11 23:38:04 -0400

    ugh

--- _archive/ansible/bootstrap_hosts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### comment out commands as needed, last command is for general upates of packages. I usually manually run this to play it safe.
-
-ansible-playbook -i ./inventory/hosts.yaml -e @secrets.yaml --ask-vault-password ./playbooks/packages-ubuntu.yaml -K
-
-ansible-playbook -i ./inventory/hosts.yaml ./playbooks/ssh.yaml -K
-
-#ansible-playbook -i ./inventory/hosts.yaml packages.yaml -K
