913682340777007407694ffef7637475406f81ef
alexrf45 <fonalex45@gmail.com>
2025-09-24 22:02:03 -0400

    clean up, homepage url fix in prod

--- _utils/upgrade.sh
+++ _utils/upgrade.sh
@@ -1,6 +1,5 @@
-#/bin/bash
-#
-#
+#!/bin/bash
+
 kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/v0.13.4/crd.yaml
 
 kubectl apply -f system-upgrade-controller.yaml
